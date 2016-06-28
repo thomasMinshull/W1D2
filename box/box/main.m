bea27f1a7b967997e4299cafacc1bfca882421c6
Thomas Minshull <thomasminshull@gmail.com>
2016-06-28 12:17:40 -0700

    no really, this time Im finished

--- box/box/main.m
+++ box/box/main.m
@@ -13,8 +13,20 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Box *cube = [[Box alloc] initWithHeight:1.0 Width:1.0 andLenght:100.1];
     
-        // insert code here...
         NSLog(@"Cube Volume: %f", cube.volume);
+        
+        Box *smallCube = [[Box alloc] initWithHeight:0.5 Width:0.5 andLenght:0.5];
+        
+        NSLog(@"Small Cube Volume: %f", smallCube.volume);
+        
+        NSLog(@"small cube can fit %d cubes",[smallCube canContainBox:cube]);
+        
+        NSLog(@"cube can fit %d cubes",[cube canContainBox:smallCube]);
+        
+        NSLog(@"This better be 1: %d", [cube canContainBox:cube]);
+        
+        NSLog(@"This better be 1: %d", [smallCube canContainBox:smallCube]);
+        
     }
     return 0;
 }
